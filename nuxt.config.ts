653974e5043c0a409af81e20df426a7ca05983bc
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2021-12-01 19:33:42 +0100

    add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  css: ['@/assets/css/app.css'],
+  build: {
+    postcss: {
+      // add Postcss options
+      postcssOptions: require('./postcss.config.js'),
+    },
+  },
 })
