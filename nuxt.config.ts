5cf8e5c51115c413da0a78b7b6943cddd30fe800
Benoit VRIGNAUD <benoit.vrignaud@tangue.fr>
2021-12-28 21:16:25 +0100

    fix loading fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  css: ['@/assets/scss/app.scss'],
+  css: [
+    '@/assets/scss/app.scss',
+    '@fontsource/nunito/400.css',
+    '@fontsource/nunito/600.css',
+    '@fontsource/nunito/700.css',
+  ],
   build: {
     postcss: {
       // add Postcss options
